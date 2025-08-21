646b1a5677cb3169d68481d32651018a18de31f5
Renata Faltova <renata.97@seznam.cz>
2025-08-21 10:38:41 +0100

    setting done

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,9 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+// pridano ze zadani
+
+// app/assets/config/manifest.js
+//= link popper.js
+//= link bootstrap.min.js
